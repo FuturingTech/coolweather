d80c467c491059d86d6e0f60759d433f5a649b79
YinHang <Futuring_Tech@163.com>
2017-08-31 15:55:10 +0800

    增加后台自动更新天气的功能

--- app/src/main/java/com/futuring/coolweather/android/WeatherActivity.java
+++ app/src/main/java/com/futuring/coolweather/android/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.futuring.coolweather.android;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -22,6 +23,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.futuring.coolweather.android.gson.Forecast;
 import com.futuring.coolweather.android.gson.Weather;
+import com.futuring.coolweather.android.service.AutoUpdateService;
 import com.futuring.coolweather.android.util.HttpUtil;
 import com.futuring.coolweather.android.util.Utility;
 
@@ -238,5 +240,7 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(carWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
